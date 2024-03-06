60449726dd7f3394e9eb66aec6cabdeb2bb8ed2a
Your Name <you@example.com>
2024-03-06 19:42:10 +0100

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,25 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+
+vim.cmd([[
+    let g:loaded_ruby_provider       = 0
+    let g:loaded_perl_provider = 0
+]])
+
+if vim.g.neovide == true then
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-+>",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-->",
+    ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
